88f69b7d2ee77a04db801e2ea490a16a6e64fa49
truongthanh1303 <truongthanh1303@gmail.com>
2022-02-26 16:30:31 +0700

    init project; add Section 1/ create query and handling loading / error state

---
+++ base-blog-em/src/Posts.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { useQuery } from 'react-query';
+
+import { PostDetail } from "./PostDetail";
+const maxPostPage = 10;
+
+async function fetchPosts() {
+  const response = await fetch(
+    "https://jsonplaceholder.typicodeabc.com/posts?_limit=10&_page=0"
+  );
+  return response.json();
+}
+
+export function Posts() {
+  const [currentPage, setCurrentPage] = useState(0);
+  const [selectedPost, setSelectedPost] = useState(null);
+
+  // replace with useQuery
+  const { data, isError, error, isLoading } = useQuery('posts', fetchPosts);
+
+  if (isLoading) {
+    return <h3>Loading ...</h3>
+  }
+
+  if (isError) {
+    return <>
+      <h3>Ooop! Something went wrong!</h3>
+      <p>{error.toString()}</p>
+    </>
+  }
+
+  if (!data) return null;
+  console.log('data ', data)
+  
+  return (
+    <>
+      <ul>
+        {data.map((post) => (
+          <li
+            key={post.id}
+            className="post-title"
+            onClick={() => setSelectedPost(post)}
+          >
+            {post.title}
+          </li>
+        ))}
+      </ul>
+      <div className="pages">
+        <button disabled onClick={() => {}}>
+          Previous page
+        </button>
+        <span>Page {currentPage + 1}</span>
+        <button disabled onClick={() => {}}>
+          Next page
+        </button>
+      </div>
+      <hr />
+      {selectedPost && <PostDetail post={selectedPost} />}
+    </>
+  );
+}
